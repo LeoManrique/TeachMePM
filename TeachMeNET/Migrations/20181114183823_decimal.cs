3e079c3318349c85ec59bd2eead9346005752d47
Leonardo Manrique <leonardomanrique43@gmail.com>
2018-11-14 13:39:45 -0500

    decimal

---
+++ TeachMeNET/Migrations/20181114183823_decimal.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeachMeNET.Migrations
+{
+    public partial class @decimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price4",
+                table: "Teachers",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price3",
+                table: "Teachers",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price2",
+                table: "Teachers",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price1",
+                table: "Teachers",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price4",
+                table: "Teachers",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price3",
+                table: "Teachers",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price2",
+                table: "Teachers",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price1",
+                table: "Teachers",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
