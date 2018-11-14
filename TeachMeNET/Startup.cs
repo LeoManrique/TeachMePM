bc32e5a3b075a7aa53780725e582614e70651e12
Leonardo Manrique <leonardomanrique43@gmail.com>
2018-11-14 00:15:33 -0500

    Update TeachMeNET/Startup.cs

--- TeachMeNET/Startup.cs
+++ TeachMeNET/Startup.cs
@@ -38,7 +38,7 @@ namespace TeachMeNET
             services.AddSession(options=>
             {
                 // Set a short timeout for easy testing.
-                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.IdleTimeout = TimeSpan.FromSeconds(300);
                 options.Cookie.HttpOnly = true;
             });
 
