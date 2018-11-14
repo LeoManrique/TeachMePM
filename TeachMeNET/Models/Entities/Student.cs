9561573e2aedcd3cd90333add915760feb74e571
Leonardo Manrique <leonardomanrique43@gmail.com>
2018-11-13 22:27:16 -0500

    entities

--- TeachMeNET/Models/Entities/Student.cs
+++ TeachMeNET/Models/Entities/Student.cs
@@ -7,5 +7,15 @@ namespace TeachMeNET.Models.Entities
 {
     public class Student
     {
+
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int Age { get; set; }
+        public string SchoolType { get; set; }
+
+        public string Interest1 { get; set; }
+        public string Interest2 { get; set; }
+        public string Interest3 { get; set; }
+
     }
 }
--- TeachMeNET/Models/Entities/Teacher.cs
+++ TeachMeNET/Models/Entities/Teacher.cs
@@ -7,5 +7,25 @@ namespace TeachMeNET.Models.Entities
 {
     public class Teacher
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int Age { get; set; }
+        public string Degree { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int ToHouse { get; set; }
+        public int MyHouse { get; set; }
+        public int PublicSpace { get; set; }
+        public int Online { get; set; }
+
+        public string Topic1 { get; set; }
+        public int Price1 { get; set; }
+        public string Topic2 { get; set; }
+        public int Price2 { get; set; }
+        public string Topic3 { get; set; }
+        public int Price3 { get; set; }
+        public string Topic4 { get; set; }
+        public int Price4 { get; set; }
+
     }
 }
